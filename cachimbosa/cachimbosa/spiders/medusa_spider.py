e2fdaa12850cfee1b9054ac5e2c598bab9df34db
Tihomir Stoychev <tihomir_alcudia3@hotmail.com>
2020-09-11 15:42:40 +0200

    the good shisha y medusa scrawleados

---
+++ cachimbosa/cachimbosa/spiders/medusa_spider.py
@@ -0,0 +1,63 @@
+import scrapy
+import json
+import re
+
+
+class MedusaSpider(scrapy.Spider):
+    name = "medusa"
+    start_urls = ['https://www.medusashishashop.com/cachimbas/']
+
+    def parse(self, response):
+        marcas = response.css('div.product-category a::attr(href)').getall()
+        for marcaLink in marcas:
+            peticionShishas = scrapy.Request(response.urljoin(
+                marcaLink), callback=self.obtenerShishas)
+            yield peticionShishas
+
+    def obtenerShishas(self, response):
+        shishas = response.css('div.col-inner > a::attr(href)').getall()
+
+        if(len(shishas) > 0):
+            for shisha in shishas:
+                peticionProducto = scrapy.Request(response.urljoin(
+                    shisha), callback=self.obtenerShishaDetalle)
+                peticionProducto.cb_kwargs['requestUrl'] = shisha
+                yield peticionProducto
+       
+
+    def obtenerShishaDetalle(self, response, requestUrl):
+        mainProduct = response.css('div.product-main')
+        footerProduct = response.css('div.product-footer')
+        titulo = mainProduct.css('h1.product-title::text')[0].get()
+        precio = mainProduct.css(
+            'span.woocommerce-Price-amount bdi::text')[0].get()
+        divisa = mainProduct.css(
+            'span.woocommerce-Price-amount bdi span::text')[0].get()
+        imagen = mainProduct.css(
+            '.woocommerce-product-gallery__image a::attr(href)').get()
+        marca = mainProduct.css('.woocommerce-breadcrumb a::text')[-1].get()
+        informacionUnidades = re.findall(
+            "\d+", mainProduct.css('p.in-stock::text')[0].get())
+        etiquetas = footerProduct.css(
+            'div.woocommerce-Tabs-panel ul li::text').getall()
+        cantidad = 0
+        # significa que ha encontrado un numero en el string, hay pocas unidades
+        if len(informacionUnidades) > 0:
+            cantidad = int(informacionUnidades[0])
+        elif len(informacionUnidades) == 0:
+            cantidad = int(mainProduct.css(
+                'div.quantity input.qty::attr(max)')[0].get())
+
+        yield {
+            'linkProducto': requestUrl,
+            'titulo': titulo,
+            'precioOriginal': precio,
+            'precioRebajado': None,
+            'divisa': divisa,
+            'imagen': imagen,
+            'marca': marca,
+            'modelo': titulo.lower(),
+            'agotado': False,
+            'cantidad': cantidad,
+            'categorias': ['cachimba'],
+            'etiquetas': etiquetas}
---
+++ cachimbosa/cachimbosa/spiders/tgs_spider.py
@@ -0,0 +1,76 @@
+import scrapy
+import json
+
+
+class TheGoodShishaSpider(scrapy.Spider):
+    name = "tgs"
+    start_urls = ['https://www.thegoodshisha.com/product-category/shishas/']
+    itemFinal = {}
+    allShishasParsed = []
+
+    def parse(self, response):
+        shishas = response.css('div.product-list-item')
+        botonRef = response.css(
+            'a.pagination-item-next-link::attr(href)')
+        if botonRef is not None and len(botonRef) > 0:
+            self.botonSiguiente = botonRef[0].get()
+        else:
+            self.botonSiguiente = None
+        self.loopInfo = {
+            'actualIndex': 0,
+            'total': len(shishas)
+        }
+        for shisha in shishas:
+            bloqueShishaA = shisha.css("a.woocommerce-LoopProduct-link")[0]
+            detalles = bloqueShishaA.css('div.kw-details')[0]
+            detallesPrecio = detalles.css('span.price')[0]
+            precios = detallesPrecio.css('span.woocommerce-Price-amount::text')
+            linkDeProducto = bloqueShishaA.css('::attr(href)')[0].get()
+
+            itemFinal = {
+                'linkProducto': linkDeProducto,
+                'marca': linkDeProducto.split("/")[-3].upper(),
+                'modelo': linkDeProducto.split("/")[-2].upper(),
+                'imagen': bloqueShishaA.css('img::attr(src)')[0].get(),
+                'titulo': detalles.css('h3.kw-details-title::text')[0].get(),
+                'divisa': detalles.css('span.woocommerce-Price-currencySymbol::text')[0].get(),
+                'precioOriginal': precios[0].get(),
+                'precioRebajado': precios[1].get() if len(precios) > 1 else None
+            }
+
+            if linkDeProducto is not None:
+                request = scrapy.Request(response.urljoin(
+                    linkDeProducto), callback=self.obtenerInfoProducto)
+                request.cb_kwargs['itemFinal'] = itemFinal
+                yield request
+
+    def obtenerInfoProducto(self, response, itemFinal):
+        mainData = response.css('div.main-data')
+        estaFueraDeStock = mainData.css('p.out-of-stock')
+        cart = mainData.css('form.cart')
+        metadatosProducto = mainData.css('div.product_meta')
+
+        if(len(estaFueraDeStock) > 0):
+            itemFinal['agotado'] = True
+        else:
+            itemFinal['agotado'] = False
+            itemFinal['cantidad'] = cart.css('input.qty::attr(max)')[0].get()
+
+        itemFinal['categorias'] = metadatosProducto.css(
+            'span.posted_in a[rel="tag"]::text').getall()
+        itemFinal['etiquetas'] = metadatosProducto.css(
+            'span.tagged_as a[rel="tag"]::text').getall()
+
+        #self.allShishasParsed.append(itemFinal)
+        yield itemFinal
+        if self.loopInfo['actualIndex'] == (self.loopInfo['total'] - 1):
+            if self.botonSiguiente is not None:
+                request = scrapy.Request(response.urljoin(
+                    self.botonSiguiente), callback=self.parse)
+                yield request
+           
+        self.loopInfo['actualIndex'] += 1
+
+    def escribirJSON(self):
+        with open(self.name+'.json', 'w', encoding='utf-8') as f:
+            json.dump(self.allShishasParsed, f, ensure_ascii=False, indent=4)
