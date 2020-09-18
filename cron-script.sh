98d4cd2c2097db601320ede8965bbd8560b6c1c7
Ubuntu <ubuntu@ip-172-31-26-53.eu-west-3.compute.internal>
2020-09-18 19:17:12 +0000

    mejoras script

--- cron-script.sh
+++ cron-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm zuloshisha.json bengalas.json hispacachimba.json medusa.json tgs.json
+(rm zuloshisha.json bengalas.json hispacachimba.json medusa.json tgs.json) || echo "no hay ficheros que eliminar"
 
 scrapy runspider cachimbosa/cachimbosa/spiders/zuloshishas_spider.py -o zuloshisha.json
 scrapy runspider cachimbosa/cachimbosa/spiders/bengala_spider.py -o bengalas.json
@@ -16,5 +16,11 @@ fi
 echo "[Success] scrawl multiple exitoso"
 echo "[INFO] iniciando subida base de datos"
 
-python3 cachimbosa/cachimbosa/scripts/file_exporter.py 10052001Tsonyo
+python3 cachimbosa/cachimbosa/scripts/file_exporter.py $1
 
+if [ "$?" != "0" ]; then
+        echo "[Error] Guardado en base de datos fallado"
+        exit 1
+fi
+
+echo "[Success] guardado en base de datos exitoso"
